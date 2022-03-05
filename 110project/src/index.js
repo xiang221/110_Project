1bf40f5217c0fe7afc5afd9a2cac675b6d56591b
Xiang <41126704+xiang221@users.noreply.github.com>
2022-03-05 11:50:07 +0800

    0305

---
+++ 110project/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*
+function app(){
+  return React.createElement("h1",null,"This is react app.");
+}
+
+ReactDOM.render(React.createElement(App)
+);
+
+*/
