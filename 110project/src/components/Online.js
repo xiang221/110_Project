2d27373c3954a0724d398d0e73c6d453b8878ccf
Xiang <41126704+xiang221@users.noreply.github.com>
2022-03-17 23:32:07 +0800

    0317

--- 110project/src/components/Online.js
+++ 110project/src/components/Online.js
@@ -5,11 +5,28 @@ const Online = () => {
 
 
   return (
+<<<<<<< Updated upstream
     <div>
     <div class="onlinePlayer"></div>
     <div class="onlinePlayer"></div>
     <div class="onlinePlayer"></div>
     <div class="onlinePlayer"></div>
+=======
+    <div className='onlinecontainer'>
+    <div className='text'>上線玩家</div>
+    <div className='player'>
+    <div className='player1'></div>
+    </div>
+    <div className='player'>
+    <div className='player2'></div>
+    </div>
+    <div className='player'>
+    <div className='player3'></div>
+    </div>
+    <div className='player'>
+    <div className='player4'></div>
+    </div>
+>>>>>>> Stashed changes
     </div>
   )
 }
--- 110project/src/pages/Game.js
+++ 110project/src/pages/Game.js
@@ -1,8 +1,16 @@
 import React from 'react'
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import Header from '../components/Header'
 =======
 import Header from 'Header'
+>>>>>>> Stashed changes
+
+=======
+import '../styles/game.css'
+import Header from '../components/Header'
+import Mail from '../components/Mail'
+import Online from '../components/Online'
 >>>>>>> Stashed changes
 
 
@@ -13,6 +21,13 @@ const Game = () => {
 =======
     <div>
       <Header/>
+<<<<<<< Updated upstream
+=======
+      <div className='container'>
+      <Online/>
+      <Mail/>
+      </div>
+>>>>>>> Stashed changes
     </div>
 >>>>>>> Stashed changes
   )
