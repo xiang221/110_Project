37232b02f7e48a099e17c2a9b007738c6476347c
Xiang <41126704+xiang221@users.noreply.github.com>
2022-04-16 19:14:08 +0800

    Update Header.js

--- 110project/src/components/Header.js
+++ 110project/src/components/Header.js
@@ -3,9 +3,6 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-    <div className='circle circle1'></div>
-    <div className='circle circle2'></div>
-    <div className='circle circle3'></div>
     </div>
   )
 }
