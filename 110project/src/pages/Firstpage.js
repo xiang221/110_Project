d850b26069a6bec94f25950943c542bd9f6ef666
Xiang <41126704+xiang221@users.noreply.github.com>
2022-05-03 20:21:50 +0800

    0503

--- 110project/src/pages/Firstpage.js
+++ 110project/src/pages/Firstpage.js
@@ -7,10 +7,18 @@ import firstVideo from '../picture/gameOpening.mp4';
 const Firstpage = () => {
   const navigate = useNavigate();
 
+  const navEventHandler = (e) => {
+    if(e.key === 'enter'){
+      console.log('click');
+      navigate('/secondpage');
+      localStorage.clear(); 
+      window.location.reload();
+    }
+  } 
 
   return (
-    <div className='indexContainer'>
-    <video autoPlay muted loop style={{ verticalAlign:'bottom'}}>
+    <div className='indexContainer'  >
+    <video autoPlay muted loop style={{ verticalAlign:'bottom'}} onKeyPress={(e) => this.navEventHandler(e)}>
     <source src={firstVideo} type='video/mp4'/>
     </video>
     </div>
