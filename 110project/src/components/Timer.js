ab595048c1eb7eebe635acd2dd40da3fcfba62f4
CHEN507 <107507029@nccu.edu.tw>
2022-05-10 02:32:32 +0800

    修正Timer的數字顯示問題

--- 110project/src/components/Timer.js
+++ 110project/src/components/Timer.js
@@ -14,8 +14,8 @@ const Timer = () => {
         // if (allSeconds <= 0) {//如果目前Index大於目標陣列長度則返回
         //     return
         // }
-        setTimeout(() => { setAllSeconds(allSeconds + 1); Time = allSeconds+1; setMinutes(Math.trunc(allSeconds / 60)>10? Math.trunc(allSeconds / 60): "0"+ Math.trunc(allSeconds / 60)); setSeconds((allSeconds % 60)>10? (allSeconds%60): "0"+(allSeconds%60)) }, 1000)//設定一定的時間後，改變當前的Index
-        // console.log("allSeconds = "+allSeconds)
+        setTimeout(() => { setAllSeconds(allSeconds + 1); Time = allSeconds+1; setMinutes(Math.trunc(allSeconds / 60)>=10? Math.trunc(allSeconds / 60): "0"+ Math.trunc(allSeconds / 60)); setSeconds((allSeconds % 60)>=10? (allSeconds%60): "0"+(allSeconds%60)) }, 1000)//設定一定的時間後，改變當前的Index
+        console.log("allSeconds = "+allSeconds)
     }, [allSeconds])
 
     return <div>{minutes}:{seconds}</div>
